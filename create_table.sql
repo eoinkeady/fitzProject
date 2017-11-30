77582e9eb677406592bbb970d0624f395267073c
Shadynasty <eoin.keady@gmail.com>
2017-11-30 20:35:08 +0000

    working prototype

--- create_table.sql
+++ create_table.sql
@@ -1,43 +1,30 @@
 CREATE TABLE hospital.patient (
-    "PATIENT_ID"     NUMBER(6,0)
-        CONSTRAINT "PAT_PK" PRIMARY KEY,
-    "FIRST_NAME"     VARCHAR2(20 BYTE),
-    "LAST_NAME"      VARCHAR2(25 BYTE)
+    patient_id     NUMBER(6,0)
+        CONSTRAINT pat_pk PRIMARY KEY,
+    first_name     VARCHAR2(20 BYTE),
+    last_name      VARCHAR2(25 BYTE)
         CONSTRAINT "PAT_LAST_NAME_NN" NOT NULL ENABLE,
-    "EMAIL"          VARCHAR2(25 BYTE)
+    email          VARCHAR2(25 BYTE)
         CONSTRAINT "PAT_EMAIL_NN" NOT NULL ENABLE,
-    "PHONE_NUMBER"   VARCHAR2(20 BYTE)
+    phone_number   VARCHAR2(20 BYTE)
 );
 
-CREATE TABLE hospital.stay (
-    "ADMISSION_ID"   NUMBER(8,0),
-    "START_DATE"     DATE DEFAULT ( SYSDATE ),
-    "END_DATE"       DATE
-        CONSTRAINT adm_end_date NOT NULL ENABLE,
-    "ROOM_ID"        NUMBER(5,0)
-        CONSTRAINT room_nn NOT NULL ENABLE,
-    "PATIENT_ID"     NUMBER(6,0)
+CREATE TABLE hospital.ward (
+    ward_id     NUMBER(5,0)
+        CONSTRAINT ward_pk PRIMARY KEY,
+    ward_name   VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE room (
-    "ROOM_ID"        NUMBER(5,0)
+CREATE TABLE hospital.room (
+    room_id        NUMBER(6,0)
         CONSTRAINT room_pk PRIMARY KEY,
-    "FLOOR_NUMBER"   NUMBER(0) NOT NULL,
-    "WARD_ID"        NUMBER(3) NOT NULL
+    floor_number   NUMBER(1,0) NOT NULL,
+    ward_id        NUMBER(5,0) NOT NULL
         CONSTRAINT ward_fk
             REFERENCES hospital.ward ( ward_id )
 );
 
-CREATE TABLE ward (
-    ward_id      NUMBER(5,0)
-        CONSTRAINT ward_pk PRIMARY KEY,
-    ward_name    VARCHAR(20) NOT NULL,
-    head_nurse   NUMBER(4) NOT NULL
-        CONSTRAINT head_nurse
-            REFERENCES hospital.nurse ( nurse_id )
-);
-
-CREATE TABLE nurse (
+CREATE TABLE hospital.nurse (
     nurse_id     NUMBER(4,0)
         CONSTRAINT nurse_pk PRIMARY KEY,
     first_name   VARCHAR(20) NOT NULL,
@@ -47,46 +34,81 @@ CREATE TABLE nurse (
             REFERENCES hospital.ward ( ward_id )
 );
 
-CREATE TABLE nurse (
-    nurse_id     NUMBER(4,0)
-        CONSTRAINT nurse_pk PRIMARY KEY,
-    first_name   VARCHAR(20) NOT NULL,
-    last_name    VARCHAR(25) NOT NULL,
-    ward_id      NUMBER(5,0) NOT NULL
-        CONSTRAINT nurse_ward
-            REFERENCES hospital.ward ( ward_id )
+CREATE TABLE hospital.department (
+    department_id     NUMBER(6,0)
+        CONSTRAINT dept_pk PRIMARY KEY,
+    department_name   VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE appointment (
-    appointment_id   NUMBER(4,0)
-        CONSTRAINT app_pk PRIMARY KEY,
-    patient_id       NUMBER(6,0) NOT NULL
-        CONSTRAINT pat_fk
-            REFERENCES hospital.patient ( patient_id ),
-    physician_id     NUMBER(3,0) NOT NULL
-        CONSTRAINT phy_fk
-            REFERENCES hospital.physicians ( physician_id ),
-    start_date       DATE NOT NULL,
-    end_date         DATE NOT NULL,
-    room_id          NUMBER(5,0) NOT NULL
-        CONSTRAINT room_fk
-            REFERENCES hospital.room ( room_id )
-);
+DROP TABLE hospital.operation;
 
-CREATE TABLE physician (
-    physician_id   NUMBER(3,0) NOT NULL,
-    first_name     VARCHAR(20) NOT NULL,
-    last_name      VARCHAR(25) NOT NULL,
-    ward_id        NUMBER(5,0) NOT NULL
+CREATE TABLE hospital.physician (
+    physician_id     NUMBER(3,0)
+        CONSTRAINT physician_pk PRIMARY KEY,
+    first_name       VARCHAR(20) NOT NULL,
+    last_name        VARCHAR(25) NOT NULL,
+    deptartment_id   NUMBER(5,0) NOT NULL
         CONSTRAINT dept_fk
             REFERENCES hospital.department ( department_id )
 );
 
-CREATE TABLE treatment (
-    physician_id   NUMBER(3,0) NOT NULL,
-    constraint     phy_fk
-        REFERENCES hospital.physician ( physician_id ),
-    procedure_id   NUMBER(3,0) NOT NULL,
-    constraint     phy_fk
-        REFERENCES hospital.procedure ( procedure_id )
+CREATE TABLE hospital.operation (
+    operation_id     NUMBER(3,0) NOT NULL
+        CONSTRAINT op_pk PRIMARY KEY,
+    procedure_name   VARCHAR(20) NOT NULL,
+    description      VARCHAR(4000)
+);
+
+CREATE TABLE hospital.treatment (
+    physician_id   NUMBER(3,0) NOT NULL
+        CONSTRAINT phy_fk
+            REFERENCES hospital.physician ( physician_id ),
+    operation_id   NUMBER(3,0) NOT NULL
+        CONSTRAINT op_fk
+            REFERENCES hospital.operation ( operation_id )
+);
+
+CREATE TABLE hospital.drug (
+    drug_id     NUMBER(8,0) NOT NULL
+        CONSTRAINT drug_pk PRIMARY KEY,
+    drug_name   VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE hospital.prescription (
+    prescription_id   NUMBER(8,0) NOT NULL
+        CONSTRAINT presc_pk PRIMARY KEY,
+    patient_id        NUMBER(6,0)
+        CONSTRAINT pat_fk
+            REFERENCES hospital.patient ( patient_id ),
+    drug_id           NUMBER(8,0)
+        CONSTRAINT drug_fk
+            REFERENCES hospital.drug ( drug_id ),
+    dose              VARCHAR2(32)
+);
+
+CREATE TABLE hospital.appointment (
+    appointment_id   NUMBER(4,0)
+        CONSTRAINT app_pk PRIMARY KEY,
+    patient_id       NUMBER(6,0) NOT NULL
+        CONSTRAINT app_pat_fk
+            REFERENCES hospital.patient ( patient_id ),
+    physician_id     NUMBER(3,0) NOT NULL
+        CONSTRAINT app_phy_fk
+            REFERENCES hospital.physician ( physician_id ),
+    start_date       DATE DEFAULT SYSDATE,
+    end_date         DATE,
+    room_id          NUMBER(5,0) NOT NULL
+        CONSTRAINT app_room_fk
+            REFERENCES hospital.room ( room_id )
+);
+
+CREATE TABLE hospital.stay (
+    admission_id   NUMBER(8,0)
+        CONSTRAINT adm_pk PRIMARY KEY,
+    start_date     DATE DEFAULT ( SYSDATE ),
+    end_date       DATE,
+    room_id        NUMBER(5,0) NOT NULL,
+    patient_id     NUMBER(6,0)
+        CONSTRAINT adm_pat_fk
+            REFERENCES hospital.patient ( patient_id )
 );
